dfec8f28e953581ca6a4bdbe004e81521e3e4739
Wladimir Gramacho <wladimirgramacho@gmail.com>
2017-10-30 21:01:14 -0200

    Updates seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+
+wladi = User.create(name: 'Wladimir', email: 'wladimirgramacho@gmail.com', password: '123456')
