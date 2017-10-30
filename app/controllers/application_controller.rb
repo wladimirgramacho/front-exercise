872acba625f0622e12876248bbb25a4c20c7dde4
Wladimir Gramacho <wladimirgramacho@gmail.com>
2017-10-30 21:13:05 -0200

    Redirect to wallet after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  ##
+  # Redirect user to his Wallet after sign in.
+  def after_sign_in_path_for(_resource)
+    wallet_url
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
-	def index		
+  before_action :authenticate_user!, only: :wallet
+
+	def index
 	end
+
+  def wallet
+    @transactions = Transaction.where(user_id: current_user)
+  end
 end
