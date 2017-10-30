6d1b9447b39d37a0dc0023a32096b2899443fee0
Wladimir Gramacho <wladimirgramacho@gmail.com>
2017-10-30 21:11:06 -0200

    Route for wallet

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'transactions/new'
+
+  get 'transactions/create'
+
   resources :coins
   devise_for :users
+  get '/wallet' => 'home#wallet'
 
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
