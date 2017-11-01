7eb4691ba356862f4eee5b14262d045d655c05da
Wladimir Gramacho <wladimirgramacho@gmail.com>
2017-11-01 18:16:28 -0200

    Link to home on navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :home_path
 
   ##
   # Redirect user to his Wallet after sign in.
   def after_sign_in_path_for(_resource)
     wallet_url
+  end
+
+  def home_path
+    if user_signed_in?
+      @home_path = wallet_url
+    else
+      @home_path = root_path
+    end    
   end
 end
